d262c41e3b04628e65dff976c15e4bcffc4c5dd9
javagg <lu.lee05@g.com>
2014-12-18 11:58:27 +0800

    move two architect-hhtp plugins

--- NodeServer/config.js
+++ NodeServer/config.js
@@ -1,11 +1,11 @@
 module.exports = [
   { 
-    packagePath: "architect-http",
+    packagePath: "./plugins/architect-http",
     port: process.env.PORT || 8080,
     host: process.env.IP || "0.0.0.0"
   },
   { 
-    packagePath: "architect-http-static", root: "www"
+    packagePath: "./plugins/architect-http-static", root: "www"
   },
   "./plugins/calculator",
   "./plugins/db",
--- NodeServer/plugins/engine.io/engine.io.js
+++ NodeServer/plugins/engine.io/engine.io.js
@@ -2,15 +2,7 @@ module.exports = function setup(options, imports, register) {
   var http = imports.http.server ;
   var engine = require('engine.io');
   var server = engine.attach(http);
-  server.on('connection', function(socket) {
-    //console.log("engine.io connected!");
-    socket.on('message', function(data) {
-    });
-    socket.on('close', function(){
-      //console.log("engine.io closed!")
-    });
-  });
-
+  
   register(null, {
       eio: {
         "server": server
