b4c2650411460761d40ac6be2c64e83b08b9f0e7
javagg <lu.lee05@g.com>
2014-12-19 12:35:51 +0800

    use signalr 2.0.1

--- Mso.SignalR/server.js
+++ Mso.SignalR/server.js
@@ -3,7 +3,7 @@ var connect = require('connect')
 var express = require('express')
 
 var app = express();
-app.use(connect.logger('dev'));
+//app.use(connect.logger('dev'));
 app.all('/signalr/*', owin(__dirname + '/bin/Debug/Mso.SignalR.dll'));
 app.use(express.static(__dirname));
 app.listen(3000);
