9854b9f7d07790e45bee23d3885f1119d7a34131
李璐 <alex@AlexMacBook.local>
2014-12-20 17:51:53 +0800

    add two hubs dd

--- Mso.SignalR/SmartQuantHub.cs
+++ Mso.SignalR/SmartQuantHub.cs
@@ -7,6 +7,80 @@ namespace Mso.SignalR.Hubs
 {
     public class SmartQuantHub : Hub<ISmartQuantService>
     {
+        public async Task<string> GetProviderListAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<string> GetProviderAsync(string providerName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<string> SetProviderPropertiesAsync(string providerName, string jsonProperties)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<string> GetProviderPropertiesAsync(string providerName, string[] names)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async void ConnectProvider(string providerName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async void DisconnectProvider(string providerName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async void GetInstrumentListAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async void AddInstrumentsAsync(string jsonText)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async void DeleteInstrumentsAsync(string[] names)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async void UpdateInstrumentAsync(string names, string jsonProperties)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<string> GetDataSeriesListAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<string> GetStrategyListAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<string> StartStrategy(string strategyId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<string> StopStrategy(string strategyId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<string> PauseStrategy(string strategyId, string evet)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
 
